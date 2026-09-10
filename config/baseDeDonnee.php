b6d4e57ebfbfa51b0f6c06cca160c214b1eb5e06
Belkahla <belcalaanis@gmail.com>
2026-09-10 11:49:14 -0400

    Modification du noms des variables d'environnements

--- config/baseDeDonnee.php
+++ config/baseDeDonnee.php
@@ -14,11 +14,11 @@ function lireVariableEnv(string $nom): string
     return $valeur;
 }
 
-$hote = lireVariableEnv("BD_HOST");
-$port = lireVariableEnv("BD_PORT");
-$nomBD = lireVariableEnv("BD_DATABASE");
-$utilisateur = lireVariableEnv("BD_NOM_UTILISATEUR");
-$motDePasse = lireVariableEnv("BD_MOT_DE_PASSE");
+$hote = lireVariableEnv("DB_HOST");
+$port = lireVariableEnv("DB_PORT");
+$nomBD = lireVariableEnv("DB_DATABASE");
+$utilisateur = lireVariableEnv("DB_USERNAME");
+$motDePasse = lireVariableEnv("DB_PASSWORD");
 
 $dsn = "mysql:host={$hote};port={$port};dbname={$nomBD};charset=utf8mb4";
 
